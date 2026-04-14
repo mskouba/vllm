287b9f4895137f1d801d825d7352dbafb9a4d879
Michael kouba <mkouba@Mac.hsd1.co.comcast.net>
2026-04-14 07:49:12 -0600

    testing

--- csrc/moe/marlin_moe_wna16/marlin_template.h
+++ csrc/moe/marlin_moe_wna16/marlin_template.h
@@ -19,11 +19,10 @@
  * Adapted from https://github.com/IST-DASLab/marlin
  */
 
-// >>> DEBUG: unconditionally enable trace for batch-invariance diagnosis <<<
-// Remove this block before shipping.
-#define MARLIN_MOE_TRACE
-#define MARLIN_MOE_TRACE_TARGET_ROW 0
-#define MARLIN_MOE_TRACE_TARGET_COL -1
+// >>> DEBUG: uncomment to enable kernel trace <<<
+// #define MARLIN_MOE_TRACE
+// #define MARLIN_MOE_TRACE_TARGET_ROW 0
+// #define MARLIN_MOE_TRACE_TARGET_COL -1
 // >>> END DEBUG <<<
 
 #ifndef MARLIN_NAMESPACE_NAME
